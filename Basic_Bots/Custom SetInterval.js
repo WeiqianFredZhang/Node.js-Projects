c704b742bf12b76cd44052ddd8cc05b184ad29db
Dream (CS Edition) <freddyzhang666@gmail.com>
2021-05-04 12:36:48 -0700

    Update Custom SetInterval.js

--- Basic_Bots/Custom SetInterval.js
+++ Basic_Bots/Custom SetInterval.js
@@ -5,7 +5,30 @@ function recursion() {
   
     // do something
   
+    // after you write if-else statements, you can customize how long and what you do in every recursion.
+  
     timeToWait = Math.round(Math.random() * (maxTime - minTime)) + minTime;
     setTimeout(recursion, timeToWait);
 }
 recursion();
+
+
+
+
+
+
+// a useful but simple built-in function
+
+const oneMinute = 60000;
+function mystery(){
+    // do something
+    // do another thing
+}
+setInterval(mystery(),oneMinute);  // the function mystery() runs once per minute
+
+
+// the above is the same as
+setInterval(()=>{
+    // do something
+    // do another thing
+},oneMinute);
