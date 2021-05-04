b6922a1dbbb77f40c0e19bf234d3469f9432626a
Dream (CS Edition) <freddyzhang666@gmail.com>
2021-05-04 12:49:04 -0700

    Update Read From json File.js

--- Basic_Bots/Read from json File.js
+++ Basic_Bots/Read from json File.js
@@ -1,11 +1,14 @@
-// The Demo config.json file used is in the Basic_Bots folder
+// The demo config.json file used is in the Basic_Bots folder
 
 const config = require('./config.json');
+client.config = config;
 
 let listOfItems = [], specialItems = [];
+const owner = config.owner;
+
 
 for (const item of config.list) {
-  listOfItems.push(item);
+  listOfItems.push(owner + item);
   if (item.substring(0,7).toLowerCase() === "special")
     specialItems.push(item);
 }
